2ea9b1c685f172a05bb4eb9e7e70fa15c206ccef
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-04 12:00:59 -0700

    Stil playing with anagrams

--- hashmapdrills.js
+++ hashmapdrills.js
@@ -50,4 +50,44 @@ function palindrome(string) {
   }
 }
 
-console.log(palindrome("acecarr"));
+// console.log(palindrome("acecarr"));
+
+//--------------------------Anagrams---------------------------------------
+
+// Write an algorithm to group a list of words into anagrams. For example,
+// if the input was ['east', 'cars', 'acre', 'arcs', 'teas', 'eats',
+//'race'], the output should be: [['east', 'teas', 'eats'], ['cars',
+//'arcs'], ['acre', 'race']].
+
+
+//Few Pointers
+//Input Type is Array
+//Output Type is Array of Arrays
+
+//Comparison - Dealing with length - if you set a string to another
+//string and the length doesn't increase - it would be an anagrams
+
+//Other ways of comparison -
+
+
+//
+
+// const anagram = function(array) {
+//   const newArray = [];
+//   const firstValue = array[0];
+//   newArray.push(firstValue);
+//
+//   //newArray = ['east'];
+//
+//   const firstHash = new HashMap();
+//
+//   //firstHash = [];
+//
+//   firstHash.set()
+//   for(let i = 0; i < array.length; i++) {
+//
+//   }
+//
+//
+//
+// }
