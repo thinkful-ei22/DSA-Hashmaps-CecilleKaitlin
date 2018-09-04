137c09774aa0f7b713c9f8500b473dbb47d5a1a3
Cecille Salazar <cecillersalazar@gmail.com>
2018-09-04 08:19:13 -0700

    Finalized hashmap class

--- hashmap.js
+++ hashmap.js
@@ -13,6 +13,66 @@ class HashMap {
         }
         return hash >>> 0;
     }
+
+    get(key) {
+        const index = this._findSlot(key);
+        if (this._slots[index] === undefined) {
+            throw new Error('Key error');
+        }
+        return this._slots[index].value;
+    }
+
+    set(key, value) {
+        const loadRatio = (this.length + 1) / this._capacity;
+        if (loadRatio > HashMap.MAX_LOAD_RATIO) {
+            this._resize(this._capacity * HashMap.SIZE_RATIO);
+        }
+
+        const index = this._findSlot(key);
+        this._slots[index] = {
+            key,
+            value
+        };
+        this.length++;
+    }
+
+    remove(key) {
+        const index = this._findSlot(key);
+        const slot = this._slots[index];
+        if (slot === undefined) {
+            throw new Error('Key error');
+        }
+        slot.deleted = true;
+        this.length--;
+        this._deleted++;
+    }
+
+    _findSlot(key) {
+        const hash = HashMap._hashString(key);
+        const start = hash % this._capacity;
+
+        for (let i=start; i<start + this._capacity; i++) {
+            const index = i % this._capacity;
+            const slot = this._slots[index];
+            if (slot === undefined || slot.key == key) {
+                return index;
+            }
+        }
+    }
+
+    _resize(size) {
+        const oldSlots = this._slots;
+        this._capacity = size;
+        // Reset the length - it will get rebuilt as you add the items back
+        this.length = 0;
+        this._slots = [];
+
+        for (const slot of oldSlots) {
+            if (slot !== undefined) {
+                this.set(slot.key, slot.value);
+            }
+        }
+    }
 }
 
 HashMap.MAX_LOAD_RATIO = 0.9;
